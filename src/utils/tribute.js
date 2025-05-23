794d8e0f3f5210ca80b9509410afc37d7e12d9c5
Andrew Wadsworth <andrew@itmooti.com>
2025-05-23 21:57:37 +1000

    Adjust reply UI

--- src/utils/tribute.js
+++ src/utils/tribute.js
@@ -8,7 +8,7 @@ export const tribute = new Tribute({
            <span class = "font-light text-sm">${item.string}</span>
          </div>`,
         selectTemplate: (item) =>
-          `<span contenteditable="false" class="mention" data-mention-id="${item.original.value}">
+          `<span contenteditable="false" class="mention bg-gray-200 px-1 rounded" data-mention-id="${item.original.value}">
            @${item.original.key}
          </span>&nbsp;`,
         values: [],
