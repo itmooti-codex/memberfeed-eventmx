b515c01853dbfd2284477b1b6d3903f3988ba6dc
dpes44 <dpes.adikari44@gmail.com>
2025-06-05 16:09:37 +0545

    changes

--- src/config.js
+++ src/config.js
@@ -53,6 +53,7 @@ export const state = {
   debounceTimer: null,
   currentUser: null,
   initialPostsLoaded: false,
+  isConnecting: false,
   ignoreNextSocketUpdate: false,
 };
 export const searchInput = document.getElementById("searchPost");
