fd5860756230debdd1c25557fb13faa63b92f7f4
Uttam Pradhan <uttampra1@gmail.com>
2025-06-25 16:57:11 +0545

    Update user avatar and name handling in UI; enhance profile display functionality

--- src/ui/user.js
+++ src/ui/user.js
@@ -1,6 +1,18 @@
 export function updateCurrentUserUI(state) {
   const avatar = document.getElementById("current-user-avatar");
   const avatarForModal = document.getElementById("current-user-avatar-modal");
+  const avatarName = document.querySelectorAll(".avatar-name");
+  const avatarPhoto = document.querySelectorAll(".avatar-photo");
+  if (avatarPhoto && state?.currentUser?.profile_image) {
+    avatarPhoto.forEach((el) => {
+      el.src = state.currentUser.profile_image;
+    });
+  }
+  if (avatarName && state?.currentUser?.display_name) {
+    avatarName.forEach((el) => {
+      el.textContent = state.currentUser.display_name;
+    });
+  }
   if (avatar && state?.currentUser?.profile_image) {
     avatar.src = state.currentUser.profile_image;
     if (avatarForModal) {
