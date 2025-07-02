16001098bf80aaa97798a4a0b2f49dcb213aabc3
Uttam Pradhan <uttampra1@gmail.com>
2025-07-02 16:32:46 +0545

    fix: update PARAM and PARAM_URL to use awsS3 parameters

--- src/credentials.js
+++ src/credentials.js
@@ -1,7 +1,8 @@
+
 export default {
     API_KEY: graphQlApiKey,
-    PARAM: awsParam,
-    PARAM_URL: awsParamUrl,
+    PARAM: awsS3Param,
+    PARAM_URL: awsS3ParamUrl,
     HTTP_ENDPOINT: graphQlApiEndpoint,
     WS_ENDPOINT: graphQlWsEndpoint,
 };
