03846d43d4213de7a39c8cfcc219272edd772679
Uttam Pradhan <uttampra1@gmail.com>
2025-07-03 11:10:52 +0545

    feat: enhance GIF picker UI with improved layout and close button

--- src/ui/gif.js
+++ src/ui/gif.js
@@ -8,7 +8,15 @@ export function initGifPicker() {
             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
           </svg>
         </div>
-        <div class="p-4 border-b flex space-x-2">
+        <div class="py-4 mx-4 border-b flex space-x-2 items-center justify-between">
+        <div class="h1">Choose a GIF</div>
+        <div class="flex items-center justify-start gap-2 rounded-[34px] bg-zinc-100 hover:bg-[var(--color-primary-shade)] transition-all p-2 cursor-pointer group " id="gif-close-btn" >
+              <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M18.8535 18.146C18.8999 18.1925 18.9368 18.2476 18.9619 18.3083C18.9871 18.369 19 18.4341 19 18.4997C19 18.5654 18.9871 18.6305 18.9619 18.6912C18.9368 18.7519 18.8999 18.807 18.8535 18.8535C18.807 18.8999 18.7519 18.9368 18.6912 18.9619C18.6305 18.9871 18.5654 19 18.4997 19C18.4341 19 18.369 18.9871 18.3083 18.9619C18.2476 18.9368 18.1925 18.8999 18.146 18.8535L14 14.7068L9.85398 18.8535C9.76017 18.9473 9.63292 19 9.50025 19C9.36758 19 9.24033 18.9473 9.14652 18.8535C9.0527 18.7597 9 18.6324 9 18.4997C9 18.3671 9.0527 18.2398 9.14652 18.146L13.2932 14L9.14652 9.85398C9.0527 9.76017 9 9.63292 9 9.50025C9 9.36758 9.0527 9.24033 9.14652 9.14652C9.24033 9.0527 9.36758 9 9.50025 9C9.63292 9 9.76017 9.0527 9.85398 9.14652L14 13.2932L18.146 9.14652C18.2398 9.0527 18.3671 9 18.4997 9C18.6324 9 18.7597 9.0527 18.8535 9.14652C18.9473 9.24033 19 9.36758 19 9.50025C19 9.63292 18.9473 9.76017 18.8535 9.85398L14.7068 14L18.8535 18.146Z" fill="#0E0E0E"></path>
+              </svg>
+            </div>
+        </div>
+        <div class="py-4 mx-4 flex space-x-2">
         <div class=" flex items-center gap-2 search-wrapper w-full !bg-white">
           <input type="text" id="gif-search-input" placeholder="Search GIFs…" class="!py-2 !pl-2 rounded text-sm w-full" />
           </div>
@@ -27,9 +35,6 @@ export function initGifPicker() {
               <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
             </svg>
           </div>
-        </div>
-        <div class="p-4 border-t flex justify-end">
-          <button id="gif-close-btn" class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none">Close</button>
         </div>
       </div>
     </div>`
