1fe656d56354bcfb953945cad5f063ed9270ac70
Kyshx315 <princeprince1052@gmail.com>
2025-07-03 14:32:55 +0545

    added h3 font and color variables in mentions to fix the design

--- src/utils/tribute.js
+++ src/utils/tribute.js
@@ -7,10 +7,10 @@ export const tribute = new Tribute({
         menuItemTemplate: (item) =>
           `<div class="mention-item flex items-center gap-4 h-6">
            <img src="${item.original.image || DEFAULT_AVATAR}" width="24" height="24" class = "rounded-full size-6 shrink-0"/>
-           <span class = "font-light text-sm ">${item.string}</span>
+           <span class = "h3 text-[var(--color-black)]">${item.string}</span>
          </div>`,
         selectTemplate: (item) =>
-          `<span contenteditable="false" class="mention"  data-mention-id="${item.original.value}">
+          `<span contenteditable="false" class="mention h3"  data-mention-id="${item.original.value}">
            ${item.original.key}
          </span> `,
         values: [],
