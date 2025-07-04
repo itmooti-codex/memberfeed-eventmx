de634022bc13903fca552f5434a5fb7d01809fec
Uttam Pradhan <85547505+uttamaPrh@users.noreply.github.com>
2025-07-04 17:54:30 +0545

    Update config.js

--- src/config.js
+++ src/config.js
@@ -26,8 +26,14 @@ export const WS_ENDPOINT =
   "";
 
 if (typeof window !== "undefined") {
-  window.awsParam = awsParam;
-  window.awsParamUrl = awsParamUrl;
+  // window.awsParam = awsParam;
+  // window.awsParamUrl = awsParamUrl;
+   if (typeof window.awsParam === "undefined") {
+    window.awsParam = awsParam;
+  }
+  if (typeof window.awsParamUrl === "undefined") {
+    window.awsParamUrl = awsParamUrl;
+  }
 }
 export const PROTOCOL = "vitalstats";
 export const SUB_ID = "feed-subscription";
