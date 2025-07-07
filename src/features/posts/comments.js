f5b1a7e9143891b68005c2e6caffd37e29b82748
Uttam Pradhan <uttampra1@gmail.com>
2025-07-07 11:21:17 +0545

    Refactor mention handling in comments and tribute components; adjust CSS for mention styling

--- src/features/posts/comments.js
+++ src/features/posts/comments.js
@@ -30,7 +30,7 @@ export function initCommentHandlers() {
       console.error("Node not found for uid", uid);
       return;
     }
-    const mentionHtml = `<span contenteditable="false" class="mention p2 " data-mention-id="${node.authorId}">${node.authorName}</span> `;
+    const mentionHtml = `<span class="mention p2 " data-mention-id="${node.authorId}">${node.authorName}</span>&nbsp;`;
 
     const nextDepth = Math.min((node.depth || 0) + 1, 2);
     const nextType = nextDepth === 1 ? "Comment" : "Reply";
@@ -53,7 +53,7 @@ export function initCommentHandlers() {
 
     <div class=" comment-form   w-full">
   <div class="flex w-full min-h-32 transtion-all border-[1px] hover:border-[var(--color-primary)] resize-y flex-col items-end justify-end gap-4 rounded-xl bg-[var(--grey-300)] px-3 py-2 focus-within:border focus-within:border-[var(--color-primary)]">
-      <div contenteditable="true" id="editor" data-placeholder="${placeholder}" class="p2 editor flex-1 resize-y justify-start self-stretch outline-none">${mentionHtml}</div>
+      <div contenteditable="true" id="editor" data-placeholder="${placeholder}" class="p2 editor flex-1 resize-y justify-start self-stretch outline-none">${mentionHtml} </div>
     <div class="flex w-full items-center justify-between">
     ${toolbarDesign}
     <div class="flex items-center gap-2">
--- src/utils/tribute.js
+++ src/utils/tribute.js
@@ -1,19 +1,17 @@
 import { DEFAULT_AVATAR } from "../config.js";
 
 export const tribute = new Tribute({
-    collection: [
-      {
-        trigger: "@",
-        menuItemTemplate: (item) =>
-          `<div class="mention-item flex items-center gap-4 h-6">
+  collection: [
+    {
+      trigger: "@",
+      menuItemTemplate: (item) =>
+        `<div class="mention-item flex items-center gap-4 h-6">
            <img src="${item.original.image || DEFAULT_AVATAR}" width="24" height="24" class = "rounded-full size-6 shrink-0"/>
            <span class = "p3 text-[var(--color-black)]">${item.string}</span>
          </div>`,
-        selectTemplate: (item) =>
-          `<span contenteditable="false" class="mention p2"  data-mention-id="${item.original.value}">
-           ${item.original.key}
-         </span> `,
-        values: [],
-      },
-    ],
-  });
+      selectTemplate: (item) =>
+        `<span contenteditable="false" class="mention p2" data-mention-id="${item.original.value}">${item.original.key}</span>`,
+      values: [],
+    },
+  ],
+});
