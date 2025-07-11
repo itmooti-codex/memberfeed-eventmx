51d9795daab06ca7fec3707e4885ef0c6dddf034
Uttam Pradhan <uttampra1@gmail.com>
2025-07-11 12:43:47 +0545

    Fix placeholder image visibility in likes modal

--- src/features/posts/likesModal.js
+++ src/features/posts/likesModal.js
@@ -34,8 +34,7 @@ export function openLikesModal(id) {
             return `<div class="flex items-center gap-2 p-2 mx-4 rounded hover:bg-[#E7F1FE] cursor-pointer"><img class="w-6 h-6 border-[1px] border-[#D9D9D9] rounded-full object-cover" src="${img}" onerror="this.onerror=null;this.src='${DEFAULT_AVATAR}'" alt="${name}"><div>${name} ${you}</div></div>`;
         })
         .join("") || `<div class="m-auto">
-         <img src=" https://static-au03.vitalstats.app/uploads/eventmx/dpYP3oQoAomii1_KBO68M.gif" class="size-[250px]"/>
-         <img src="https://static-au03.vitalstats.app/uploads/eventmx/dva4KMb33sPYMOlHVhvlR.jpg" class="!hidden size-[250px]"/>
+         <img src="https://static-au03.vitalstats.app/uploads/eventmx/dva4KMb33sPYMOlHVhvlR.jpg" class="size-[250px]"/>
         </div>`;
     modal.classList.remove("hidden");
     disableBodyScroll();
